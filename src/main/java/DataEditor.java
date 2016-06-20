b37ab820f3c5181b7401d4d5c72af01109cc5a05
iboryczka <iga.boryczka@gmail.com>
2016-06-21 00:12:35 +0200

    Saver + Editor
    + Dodanie zapisu danych do pliku
    + Edycja danych: merge tabel i średnia temp.

--- src/main/java/DataEditor.java
+++ src/main/java/DataEditor.java
@@ -1,5 +1,63 @@
 
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class DataEditor {
+
+    public Map<String, double[]> mergeData(List<Map<String, double[]>> dataList) {
+
+        Map<String, double[]> data;
+        int numberOfEntries = dataList.size();
+        //przypisujemy mapie 1. mapę dla ułatwienia
+        data = dataList.get(0);
+        //sprawdzamy klucze w 1. hashmapie (wszystkie sa identyczne)
+        //uzycie tego keysetu przy zapisie do jednej hashmapy
+        Set<String> keySet = data.keySet();
+
+        // iteracja po liście, pobranie z niej hashmap, policzenie średniej
+        // zapis do hashmapy
+        for (int i = 1; i < dataList.size(); i++) {
+            // pobranie wartości #mapy, dodatnie do nowej tablicy
+            // iteracja po kluczu
+            String key;
+            Iterator<String> it = keySet.iterator();
+            while (it.hasNext()) {
+                key = it.next();
+                // pobranie wartości głównej
+                double [] mainArray = data.get(key);
+                // pobranie obecnej wartości
+                double [] secondaryArray = dataList.get(i).get(key);
+                //dodanie 2 tablic
+                for (int j = 0; j < mainArray.length; j++) {
+                    mainArray[j] += secondaryArray[j];
+                }
+                data.replace(key, mainArray);
+
+            }
+
+        }
+        //pętla po wartościach, wyliczenie średniej
+        Set<String> ks = data.keySet();
+        Iterator<String> it = ks.iterator();
+        while (it.hasNext()) {
+            String key = it.next();
+            double [] array = data.get(key);
+            for (int i = 0; i < array.length; i++) {
+                //zaokrąglenie
+                array[i] = new BigDecimal(array[i]/numberOfEntries)
+                        .setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+            }
+            data.replace(key, array);
+        }
+        
+        return data;
+    }
     
+    
+    
+
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,8 @@
 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
@@ -7,15 +10,36 @@ public class main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
                
         String n = "Warszawa";
+        String n2 = "Szczecin";
         Map<String, double[]> dataMap = new HashMap<>();
+        Map<String, double[]> dataMap2 = new HashMap<>();
         DataRetriever dr = new DataRetrieverWunderground();
         DataRetriever dr2 = new DataRetrieverForecastIO();
+        List<City> lista = new ArrayList<>();
         City c = dr.getCity(n);
-        dataMap = dr2.getData(c);
-        System.out.println(c.toString());
+        City c2 = dr.getCity(n2);
+        //System.out.println(c.toString());
+        
+        List<Map<String, double[]>> dataList = new ArrayList<>();
+        dataList.add(dr2.getData(c));
+        dataList.add(dr.getData(c));
+        
+        List<Map<String, double[]>> dataList2 = new ArrayList<>();
+        dataList2.add(dr2.getData(c2));
+        dataList2.add(dr.getData(c2));
+        
+        DataEditor de = new DataEditor();
+        c.temperatureData = de.mergeData(dataList);
+        c2.temperatureData = de.mergeData(dataList2);
+        
+        lista.add(c);
+        lista.add(c2);
+        
+        DataSaver saver = new DataSaver();
+        saver.saveData(lista);
         
     }
 }
